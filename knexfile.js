98efd29f51da1676f73cb6d4f037624288507628
Pere <pere.solaclaver@gmail.com>
2019-11-08 08:28:56 +0000

    Edit knexconfig file with migrations and seed directory

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,13 @@ module.exports = {
     client: "sqlite3",
     connection: {
       filename: "./data/projects.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
