4459931dc384aebf4f918892b81384fb19e755a3
Pere <pere.solaclaver@gmail.com>
2019-11-08 10:27:46 +0000

    get and post method for resources

--- helpers/resourcesRouter.js
+++ helpers/resourcesRouter.js
@@ -10,11 +10,20 @@ router.get("/", async (req, res) => {
     const resources = await Resources.getResources();
     res.status(200).json(resources);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: `There was an error retrieving resources: ${error.message}`
-      });
+    res.status(500).json({
+      message: `There was an error retrieving resources: ${error.message}`
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const resource = await Resources.insertResource(req.body);
+    res.status(200).json(resource);
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error savign the resource: ${error.message}`
+    });
   }
 });
 
