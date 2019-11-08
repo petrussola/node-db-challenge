d30a4ed6f384fb99d990a12c242ed14a9d20279a
Pere <pere.solaclaver@gmail.com>
2019-11-08 10:05:48 +0000

    POST endpoint for projects

--- helpers/projectsModel.js
+++ helpers/projectsModel.js
@@ -1,9 +1,25 @@
 const db = require("../data/db-config");
 
 module.exports = {
-  getProjects
+  getProjects,
+  getProjectById,
+  insertProject
 };
 
 function getProjects() {
   return db("projects");
+}
+
+function getProjectById(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
+
+function insertProject(project) {
+  return db("projects")
+    .insert(project)
+    .then(id => {
+      return getProjectById(id[0]);
+    });
 }
--- helpers/projectsRouter.js
+++ helpers/projectsRouter.js
@@ -23,4 +23,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const project = await Projects.insertProject(req.body);
+    res
+      .status(200)
+      .json({
+        ...project,
+        is_completed: project.is_completed === 0 ? false : true
+      });
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error saving the project: ${error.message}`
+    });
+  }
+});
+
 module.exports = router;
