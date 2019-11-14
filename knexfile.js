e69be2ffbff9a1fecd4e1c0135bfb42a5f58696a
Pere <pere.solaclaver@gmail.com>
2019-11-14 15:45:32 +0000

    adds pg support

--- knexfile.js
+++ knexfile.js
@@ -13,5 +13,16 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
